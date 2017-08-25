159ad5171a21791b34a3993992fcad0bb10fca75
Gustavo <Amaralgustavo59@gmail.com>
2017-08-25 10:32:35 -0300

    CustomEditor com array

---
+++ AV1 Rec/Assets/Editor/MyCustomBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestMonoBehavior))]
+public class MyCustomBehavior : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+		TestMonoBehavior tmb = target as TestMonoBehavior; 
+
+		for (int i = 0; i < tmb.entrylist.Count; i++) {
+			
+			tmb.entrylist [i].value = EditorGUILayout.IntField (tmb.entrylist [i].key, tmb.entrylist [i].value);
+		}
+
+
+	} 
+
+}
---
+++ AV1 Rec/Assets/TestMonoBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMonoBehavior : MonoBehaviour {
+
+	[System.Serializable]
+	public class Entry{
+
+		public int value; 
+		public string key; 
+	}
+
+	public List<Entry> entrylist = new List<Entry>();
+
+	void Reset(){
+		
+		entrylist.Add (new Entry { value = 20, key = "matar"});
+		entrylist.Add (new Entry { value = 70, key = "denovo" });
+		entrylist.Add (new Entry { value = 100, key = "morrer" });
+	}
+}
